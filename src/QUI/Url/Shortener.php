e78245f623e40a327d7f4f32ba072841d2de0ae4
Henning Leutz <leutz@pcsg.de>
2016-02-12 07:32:41 +0100

    grundlagen klassen

---
+++ src/QUI/Url/Shortener.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * This file contains QUI\Url\Shortener
+ */
+namespace QUI\Url;
+
+use QUI;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class Shortener
+ * @package QUI\Url
+ */
+class Shortener
+{
+    /**
+     * Return the database table name
+     *
+     * @return string
+     */
+    public static function getDataBaseTableName()
+    {
+        return QUI::getDBTableName('urlshortener');
+    }
+
+    /**
+     * Event : on request
+     *
+     * @param QUI\Rewrite $Rewrite
+     * @param string $url
+     */
+    public static function onRequest($Rewrite, $url)
+    {
+        // media files are irrelevant
+        if (strpos($url, 'media/cache') !== false) {
+            return;
+        }
+
+        if (empty($url)) {
+            return;
+        }
+
+        $result = QUI::getDataBase()->fetch(array(
+            'from' => self::getDataBaseTableName(),
+            'where' => array(
+                'shortened' => $url
+            )
+        ));
+
+        if (!isset($result[0])) {
+            return;
+        }
+
+        $Redirect = new RedirectResponse($result[0]['url']);
+        $Redirect->setStatusCode(Response::HTTP_SEE_OTHER);
+
+        echo $Redirect->getContent();
+        $Redirect->send();
+        exit;
+    }
+
+    /**
+     * Add a new url and return the shorten link
+     *
+     * @param string $url
+     * @return string
+     */
+    public function addUrl($url)
+    {
+        $random = $this->random();
+
+        QUI::getDataBase()->insert(
+            $this->getDataBaseTableName(),
+            array(
+                'shortened' => $random,
+                'url' => $url
+            )
+        );
+
+        return HOST . URL_DIR . $random;
+    }
+
+    /**
+     * Return random shortend
+     *
+     * @return string
+     */
+    public function random()
+    {
+        $random = substr(
+            str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+            0,
+            10
+        );
+
+        $result = QUI::getDataBase()->fetch(array(
+            'from' => $this->getDataBaseTableName(),
+            'where' => array(
+                'shortened' => $random
+            )
+        ));
+
+        if (!isset($result[0])) {
+            return $random;
+        }
+
+        return $this->random();
+    }
+}
